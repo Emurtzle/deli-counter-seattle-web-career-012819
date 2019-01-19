4839a4ab9b7f910ce32868e2dded51174e0564b2
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 00:42:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    print "The line is currently: "
+    puts "The line is currently: "
 
     index = 1;
 
@@ -14,3 +14,6 @@ def line (array)
     end
   end
 end
+
+The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
+The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
