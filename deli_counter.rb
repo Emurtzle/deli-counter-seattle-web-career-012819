95c6aba2e21b9131743844ba9419962d1e809a35
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 00:54:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,15 @@ def line (array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    line = "The line is currently: "
+    line = "The line is currently:"
 
     index = 1;
 
     array.each do |name|
-      line.concat("#{index}. #{name} ")
+      line.concat(" ")
+      line.concat("#{index}. #{name}")
       index += 1
     end
-    line = line(0..-1)
     puts line
   end
 end
