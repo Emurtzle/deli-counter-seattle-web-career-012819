25e9f76268d8e9ee747ee46c28dd79e84dec0188
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 00:43:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,11 +9,8 @@ def line (array)
     index = 1;
 
     array.each do |name|
-      print "#{index}. #{name} "
+      puts "#{index}. #{name} "
       index += 1
     end
   end
 end
-
-The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
-The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
